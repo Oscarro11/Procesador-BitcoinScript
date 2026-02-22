0055dd8f70e4ee3797ac80f973fcbca55aa1c705
Oscarro11 <oscarrodrigomelchore@gmail.com>
2026-02-22 17:42:35 -0600

    feat: main App complete, project covers main functions properly

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,55 +1,102 @@
 package com.example;
 
-import java.util.Arrays;
-
-import com.example.OPCODES.Hash160;
-import com.example.OPCODES.OPCODE;
+import java.util.Scanner;
 
 public class App {
-
-    public static boolean traceMode = false;
+    private static boolean traceMode = false;
+    private static Scanner teclado = new Scanner(System.in);
+    private static Controlador controlador = new Controlador();
 
     public static void main(String[] args) throws Exception {
-
+        boolean continuar = true;
+        int opcion = 0;
+        
         if (args.length > 0 && args[0].equals("--trace")) {
             traceMode = true;
         }
 
-        ScriptStack stack = new ScriptStack();
+        escribir("Bienvenido al simulador de interprete de BitScript");
+        while (continuar) {
+            escribir("""
+                
+                Seleccione una opcion del menu: 
+                
+                1. Elegir archivos .txt con firma y llave publica
+                2. Evaluar firma con llave publica
+                3. Salir
+                """
+                );
+            opcion = teclado.nextInt();
+            teclado.nextLine();
+            switch (opcion) {
+                case 1:
+                    String rutaFirma = "";
+                    String rutaLlave = "";
+                    
+                    escribir("Ingrese la ruta absoluta del archivo .txt correspondiente a la firma");
+                    rutaFirma = teclado.nextLine();
 
-        byte[] signature = new byte[]{6,7,8,9,10};
-        byte[] publicKey = new byte[]{6,7,8,9,10};
+                    escribir("Ingrese la ruta absoluta del archivo .txt correspondiente a la llave publica");
+                    rutaLlave = teclado.nextLine();
 
-        stack.pushItem(signature);
-        stack.pushItem(publicKey);
+                    try {
+                        controlador.obtenerFirmaYLlave(rutaFirma, rutaLlave);
+                        escribir("Ambas rutas se han cargado adecuadamente al sistema\n");
+                    } catch (IllegalArgumentException e) {
+                        escribir("La ruta de la firma y/o de la llave publica son incorrectas. Intentelo de nuevo");
+                    }
+                    break;
 
-        byte[] pubKeyHash = Hash160.hash160(publicKey);
+                case 2:
+                    if (!traceMode) {
+                        boolean askTrace = true;
 
-        execute(OPCODE.OP_DUP, stack);
-        execute(OPCODE.OP_HASH160, stack);
+                        while (askTrace) {
+                            escribir("¿Desea usar el modo --trace para realizar la operacion? (y/n)");
+                            switch (teclado.nextLine().strip()) {
+                                case "y", "Y": 
+                                    traceMode = true;
+                                    askTrace = false; 
+                                    break;
 
-        stack.pushItem(pubKeyHash);
-        if (traceMode) {
-            System.out.println("PUSHDATA (pubKeyHash)");
-            stack.printStackState();
-        }
+                                case "n", "N": 
+                                    traceMode = false; 
+                                    askTrace = false;
+                                    break;
 
-        execute(OPCODE.OP_EQUALVERIFY, stack);
-        execute(OPCODE.OP_CHECKSIG, stack);
+                                default: 
+                                    escribir("El caracter ingresado no corresponde con 'y' o 'n', intentelo de nuevo");
+                                    break;
+                            }
+                        }
+                    }
 
-        byte[] result = stack.popItem();
+                    try {
+                        if (controlador.evaluarTransaccion(traceMode)){
+                            escribir("La transaccion puede realizarse con exito, accediendo a los fondos");
+                        }
+                        else{
+                            escribir("La firma no es adecuada para la llave publica, por lo que no se puede acceder a los fondos");
+                        }
+                        
+                    } catch (Exception e) {
+                        escribir("La firma no es adecuada para la llave publica, por lo que no se puede acceder a los fondos");
+                    }
+                    break;
 
-        if (!traceMode) {
-            System.out.println("Final result: " + Arrays.toString(result));
+                case 3:
+                    escribir("Gracias por usar el programa, esperamos volver a verlo pronto");
+                    continuar = false;
+                    break;
+            
+                default:
+                    escribir("La opcion ingresada no corresponde con alguna de las del menu, intentelo de nuevo\n");
+                    break;
+            }
         }
     }
 
-    private static void execute(OPCODE opcode, ScriptStack stack) throws Exception {
-        opcode.aplicar(null, stack);
-
-        if (traceMode) {
-            System.out.println(opcode.name());
-            stack.printStackState();
-        }
+    private static void escribir(String texto){
+        System.out.println(texto);
     }
 }
