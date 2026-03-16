989a7cca06b65fe1b9cbd48160a54e69464129a5
Oscarro11 <oscarrodrigomelchore@gmail.com>
2026-03-16 07:46:36 -0600

    test: added tests for IF structure in opcodes

--- src/test/java/com/example/ControladorTest.java
+++ src/test/java/com/example/ControladorTest.java
@@ -15,4 +15,40 @@ public class ControladorTest {
 
         Assertions.assertTrue(controlador.evaluarTransaccion(false));
     }
+
+    //Este test evalua un script que usa if, pero que no tiene hash160
+    @Test
+    public void combinacion2(){
+        Controlador controlador = new Controlador();
+        String rutaFirma = "src/test/resources/firma2.txt";
+        String rutaLlave = "src/test/resources/llave2.txt";
+
+        controlador.obtenerFirmaYLlave(rutaFirma, rutaLlave);
+
+        Assertions.assertTrue(controlador.evaluarTransaccion(false));
+    }
+
+    //Este test evalua script con if y con hash160
+    @Test
+    public void combinacion3(){
+        Controlador controlador = new Controlador();
+        String rutaFirma = "src/test/resources/firma3.txt";
+        String rutaLlave = "src/test/resources/llave3.txt";
+
+        controlador.obtenerFirmaYLlave(rutaFirma, rutaLlave);
+
+        Assertions.assertTrue(controlador.evaluarTransaccion(false));
+    }
+
+    //Este test evalua script con ifs anidados y con hash160
+    @Test
+    public void combinacion4(){
+        Controlador controlador = new Controlador();
+        String rutaFirma = "src/test/resources/firma4.txt";
+        String rutaLlave = "src/test/resources/llave4.txt";
+
+        controlador.obtenerFirmaYLlave(rutaFirma, rutaLlave);
+
+        Assertions.assertTrue(controlador.evaluarTransaccion(false));
+    }
 }
